f7fc28b00fc50a06080ddca135c63199150ecbbf
Marko Karajlovic <markokarajlovic@gmail.com>
2018-01-17 20:04:50 +0100

    Drugi commit: PrazanActivity i SettingsActivity
    Kretanje za buttonom sa main na prazan

---
+++ app/src/main/java/com/example/laptop/androidtestapp/PrazanActivity.java
@@ -0,0 +1,13 @@
+package com.example.laptop.androidtestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrazanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prazan);
+    }
+}
