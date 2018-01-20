f3c8b2428531ac8988445555dc3e3d3e4c993c2b
Marko Karajlovic <markokarajlovic@gmail.com>
2018-01-20 21:12:55 +0100

    4

--- app/src/main/java/com/example/laptop/androidtestapp/MainActivity.java
+++ app/src/main/java/com/example/laptop/androidtestapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.laptop.androidtestapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,12 +16,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import static com.example.laptop.androidtestapp.R.id.probafragmet;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private FrameLayout fragmentContainer;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        fragmentContainer= findViewById(probafragmet);
+        button = findViewById(R.id.button2);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -103,5 +118,19 @@ public class MainActivity extends AppCompatActivity
     public void KliknuoSam(View view) {
         Intent intent=new Intent(this,PrazanActivity.class);
         startActivity(intent);
+    }
+
+
+    @SuppressLint("ResourceType")
+    public void openFragment(View view) {
+
+        ProbaFragment objfragment = ProbaFragment.newInstance();
+
+        FragmentManager fm= getSupportFragmentManager();
+        FragmentTransaction transaction=fm.beginTransaction();
+
+        transaction.setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_right,R.anim.enter_from_right,R.anim.exit_to_right);
+        transaction.addToBackStack(null);
+        transaction.add(R.id.probafragmet,objfragment,"ProbaFragment").commit();
     }
 }
